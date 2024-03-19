c80910d115479cd0c3e30b42d874c0d6ad416972
jjookki33 <jjookki33@gmail.com>
2024-03-19 23:55:24 +0900

    🚀 Initial project
    Initial project setting

---
+++ src/main/java/com/goorm/behindyou/BehindyouApplication.java
@@ -0,0 +1,13 @@
+package com.goorm.behindyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BehindyouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BehindyouApplication.class, args);
+    }
+
+}
